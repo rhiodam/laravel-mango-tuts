0744175ba78bf7340e8a8034e32d6c83c121ec2b
Rhioda muthie <rhiodam@gmail.com>
2019-04-29 17:41:17 +0700

    Pengguna and Telepon mcr plus seeder

---
+++ app/Telepon.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Telepon extends Model
+{
+    //
+    protected $table = "telepon";
+
+    public function pengguna()
+    {
+        return $this->belongsTo('App\Pengguna');
+    }
+
+}

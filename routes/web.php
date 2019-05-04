b3c5a4c9f9a841d2f194c2164291ea4a815b164a
Rhioda muthie <rhiodam@gmail.com>
2019-05-04 20:30:22 +0700

    #28 Enkripsi dan Dekripsi

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/pegawai', 'PegawaiController@index');
 
+//Route::resource('pegawai','PegawaiController');
 
 Route::resource('pengguna','PenggunaController');
+Route::resource('artikel','WebController');
+
+
+Route::resource('guru','GuruController');
+Route::get('/guru/hapus/{id}', 'GuruController@hapus');
+Route::get('/guru/trash', 'GuruController@trash');
+
+Route::get('/enkripsi/', 'UtilitiesController@enkripsi');
+
+Route::get('/data/', 'UtilitiesController@data');
+Route::get('/data/{data_rahasia}', 'UtilitiesController@data_proses');
+
+
+Route::get('/artikel', 'WebController@index');
